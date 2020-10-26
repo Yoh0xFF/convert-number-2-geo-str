8bad0b76bd1378be698461a95b4ed631a2909868
Ioram Gordadze <Yoh0xFF@gmail.com>
2020-10-26 14:19:13 +0400

    Upgrade dependency versions.

--- src/test/java/core/NumberToGeoStrTest.java
+++ src/test/java/core/NumberToGeoStrTest.java
@@ -2,8 +2,9 @@ package core;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class NumberToGeoStrTest {
     
